6512a9aa1c6fe08ec27902c611d165a32a5be030
Isaac <imalemelo@yahoo.com>
2024-08-23 19:40:30 +0200

    provinces and cover fixed

--- app/Filament/Resources/ExamPaperResource/Pages/ListExamPapers.php
+++ app/Filament/Resources/ExamPaperResource/Pages/ListExamPapers.php
@@ -9,6 +9,7 @@ use App\Models\ExamPaper;
 use App\Models\ExamQuestion;
 use App\Models\ExamTotalQuestion;
 use App\Models\Program;
+use App\Models\Province;
 use Filament\Actions;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
@@ -42,98 +43,107 @@ class ListExamPapers extends ListRecords
 
                 ])
                 ->action(function ($data) {
-                    $new_ref_number = $this->generateRefNumber();
+                    // Get all provinces
+                    $provinces = Province::all();
 
-                    Log::info("Ref Number is ".$new_ref_number." and Program ID is ".$data['program_id']);
+                    // Loop through each province to generate an exam paper
+                    foreach ($provinces as $province) {
+                        // Generate the reference number with the province code
+                        $new_ref_number = $this->generateRefNumber($province->code);
 
-                    // Get total count of questions
-                    $totalProgramQuestions = ExamQuestion::where('program_id', $data['program_id'])->count();
+                        Log::info("Generating exam paper with Ref Number " . $new_ref_number . " for Province " . $province->name . " and Program ID is " . $data['program_id']);
 
-                    // $totalExamPaperQuestions = ExamTotalQuestion::where("name", "default")->first()->total_questions;
-                     // Explicitly set the total number of questions to generate to 150
-                    $totalExamPaperQuestions = 150;
+                        // Get total count of questions for the program
+                        $totalProgramQuestions = ExamQuestion::where('program_id', $data['program_id'])->count();
 
-                    Log::info("The Exam questions ".$totalProgramQuestions ." and Total Exam Questions ".$totalExamPaperQuestions);
+                        // Explicitly set the total number of questions to generate to 150
+                        $totalExamPaperQuestions = 150;
 
-                    // Check if there are enough questions in the database
-                    if ($totalProgramQuestions < $totalExamPaperQuestions) {
-                        // Handle case when there are not enough questions
-                        //echo "Error: There are not enough questions in the database.";
-                        Log::info("The Exam questions ".$totalProgramQuestions ." is less than the intended ".$totalExamPaperQuestions);
-                        return;
-                    }
-
-                    // Initialize an array to store selected question IDs
-                    $selectedQuestionIds = [];
-
-                    //get program competences
-                    $program_competences = Competency::where("program_id", $data['program_id'])->get();
-
-                    Log::info("Program Competences",["data",json_encode($program_competences)]);
-
-                    foreach ($program_competences as $program_competence) {
-                        //total question count per competency
-
-                        // Step 2: Convert to a float
-                        $percentageValue = floatval($program_competence->weight);
-
-                        // Step 3: Divide by 100 to get the decimal value
-                        $percentageDecimal = $percentageValue / 150;
-                        $total_competence_questions = $percentageDecimal * $totalExamPaperQuestions;
-
-                        Log::info("Total Competence_questions => " . $total_competence_questions);
-
-                        // Loop to select unique question IDs per competency
-                        while (count($selectedQuestionIds) < $total_competence_questions) {
-                            // Generate a random question ID
-                            $randomQuestionId = rand(1, $total_competence_questions);
-
-                            // Check if the selected question ID is not already in the array
-                            if (!in_array($randomQuestionId, $selectedQuestionIds)) {
-                                // Add the unique question ID to the array
-                                $selectedQuestionIds[] = $randomQuestionId;
-                            }
+                        // Check if there are enough questions in the database
+                        if ($totalProgramQuestions < $totalExamPaperQuestions) {
+                            Log::info("The Exam questions " . $totalProgramQuestions . " is less than the intended " . $totalExamPaperQuestions . " for Province " . $province->name);
+                            continue; // Skip to the next province if not enough questions
                         }
-                    }
 
+                        // Initialize an array to store selected question IDs
+                        $selectedQuestionIds = [];
 
-                    // Loop to insert selected questions into the ExamPaper table
-                    foreach ($selectedQuestionIds as $questionId) {
-                        // Retrieve the question from the ExamQuestions table
-                        $question = ExamQuestion::find($questionId);
+                        // Get program competencies
+                        $program_competences = Competency::where("program_id", $data['program_id'])->get();
 
-                        // Create a new ExamPaper record and populate its fields
-                        $examPaper = new ExamPaper();
-                        $examPaper->ref_number = $new_ref_number;
-                        $examPaper->exam_sitting_date = $data["exam_sitting_date"];
-                        $examPaper->program_id = $question->program_id;
-                        $examPaper->competency_id = $question->competency_id;
-                        $examPaper->year = $question->year;
-                        $examPaper->month = $question->month;
-                        $examPaper->image = $question->image;
-                        $examPaper->question = $question->question;
-                        $examPaper->option_a = $question->option_a;
-                        $examPaper->option_b = $question->option_b;
-                        $examPaper->option_c = $question->option_c;
-                        $examPaper->option_d = $question->option_d;
-                        $examPaper->option_e = $question->option_e;
-                        $examPaper->correct_answer = $question->correct_answer;
-                        $examPaper->user_id = Auth::user()->id;
+                        Log::info("Program Competences", ["data", json_encode($program_competences)]);
 
-                        // Save the ExamPaper record
-                        $examPaper->save();
+                        foreach ($program_competences as $program_competence) {
+                            // Convert the weight to a decimal
+                            $percentageDecimal = floatval($program_competence->weight) / 100;
+
+                            // Calculate the number of questions for this competency
+                            $total_competence_questions = ceil($percentageDecimal * $totalExamPaperQuestions);
+
+                            Log::info("Total Competence_questions => " . $total_competence_questions);
+
+                            // Fetch the questions from the database for this competency
+                            $questions = ExamQuestion::where('competency_id', $program_competence->id)
+                                ->where('program_id', $data['program_id'])
+                                ->inRandomOrder()
+                                ->take($total_competence_questions)
+                                ->pluck('id')
+                                ->toArray();
+
+                            // Merge the fetched question IDs into the selected question IDs array, ensuring uniqueness
+                            $selectedQuestionIds = array_merge($selectedQuestionIds, array_diff($questions, $selectedQuestionIds));
+                        }
+
+                        // Ensure that exactly 150 questions are selected
+                        $selectedQuestionIds = array_slice($selectedQuestionIds, 0, $totalExamPaperQuestions);
+
+                        if (count($selectedQuestionIds) < $totalExamPaperQuestions) {
+                            Log::error("Error: Less than the required number of questions selected for Province " . $province->name);
+                            continue; // Skip to the next province if not enough questions
+                        }
+
+                        Log::info("Success: Correct number of questions selected for Province " . $province->name);
+
+                        // Loop to insert selected questions into the ExamPaper table
+                        foreach ($selectedQuestionIds as $questionId) {
+                            // Retrieve the question from the ExamQuestions table
+                            $question = ExamQuestion::find($questionId);
+
+                            // Create a new ExamPaper record and populate its fields
+                            $examPaper = new ExamPaper();
+                            $examPaper->ref_number = $new_ref_number;
+                            $examPaper->exam_sitting_date = $data["exam_sitting_date"];
+                            $examPaper->program_id = $question->program_id;
+                            $examPaper->competency_id = $question->competency_id;
+                            $examPaper->year = $question->year;
+                            $examPaper->month = $question->month;
+                            $examPaper->image = $question->image;
+                            $examPaper->question = $question->question;
+                            $examPaper->option_a = $question->option_a;
+                            $examPaper->option_b = $question->option_b;
+                            $examPaper->option_c = $question->option_c;
+                            $examPaper->option_d = $question->option_d;
+                            $examPaper->option_e = $question->option_e;
+                            $examPaper->correct_answer = $question->correct_answer;
+                            $examPaper->user_id = Auth::user()->id;
+                            $examPaper->province_id = $province->id; // Store the province ID with the exam paper
+
+                            // Save the ExamPaper record
+                            $examPaper->save();
+                        }
+
+                        Log::info("Exam Paper Generated Successfully for Province " . $province->name);
                     }
 
                     Notification::make()
                         ->success()
-                        ->title('Exam Paper ' . $new_ref_number)
-                        ->body('Exam Paper Generated Successfully');
-
-                    //echo "Exam paper generated successfully with 15 unique questions.";
+                        ->title('Exam Papers')
+                        ->body('Exam Papers Generated Successfully for all Provinces');
 
                     $this->redirect("exam-papers");
-
                 })
+
+
 
         ];
     }
